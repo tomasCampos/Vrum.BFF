62ced2346b204746344bb03572153cb33b5ff5f4
tomas <tomasantoniocampos@gmail.com>
2021-02-16 18:56:28 -0300

    Configurando conexão com banco de dados

---
+++ Repositorio/Constantes/AppConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Repositorio.Constantes
+{
+    public class AppConstants
+    {
+        public const string CONNECTION_STRING = "server=us-cdbr-east-03.cleardb.com;user=bf9c7fe9712622;password=e459392c;database=heroku_3dc1bcc8f5cdd12;SSL Mode=None";
+    }
+}
---
+++ Repositorio/Infraestrutura/DataBaseConnector.cs
@@ -0,0 +1,64 @@
+﻿using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Repositorio.Constantes;
+
+namespace Repositorio.Infraestrutura
+{
+    class DataBaseConnector
+    {
+        private readonly MySqlConnection _conn;
+
+        public DataBaseConnector()
+        {
+            _conn = new MySqlConnection(AppConstants.CONNECTION_STRING);
+        }
+
+        public async Task<IEnumerable<T>> SelecionarAsync<T>(string sql)
+        {
+            await _conn.OpenAsync();
+            
+            var result = await _conn.QueryAsync<T>(sql);
+
+            await _conn.CloseAsync();
+
+            return result;
+        }
+
+        public async Task<IEnumerable<T>> SelecionarAsync<T>(string sql, object parametros)
+        {
+            await _conn.OpenAsync();
+
+            var result = await _conn.QueryAsync<T>(sql, parametros);
+            
+            await _conn.CloseAsync();
+
+            return result;
+        }
+
+        public async Task<int> ExecutarAsync(string sql)
+        {
+            await _conn.OpenAsync();
+
+            var numeroDeLinhasAfetadas = await _conn.ExecuteAsync(sql);
+
+            await _conn.CloseAsync();
+
+            return numeroDeLinhasAfetadas;
+        }
+
+        public async Task<int> ExecutarAsync(string sql, object parametros)
+        {
+            await _conn.OpenAsync();
+            
+            var numeroDeLinhasAfetadas = await _conn.ExecuteAsync(sql, parametros);
+            
+            await _conn.CloseAsync();
+
+            return numeroDeLinhasAfetadas;
+        }
+    }
+}
