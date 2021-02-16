75ff5b3f0858884a5ab7337a9530e70e18eec04a
tomas <tomasantoniocampos@gmail.com>
2021-02-16 18:10:35 -0300

    Inicial Commit. Configurando API

---
+++ Vrum.BFF/Startup.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using MySqlConnector;
+
+namespace Vrum.BFF
+{
+    public class Startup
+    {
+        public readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddTransient(_ => new MySqlConnection(Configuration["ConnectionStrings:Default"]));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("*")
+                        .AllowAnyHeader()
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod();
+                });
+            });
+
+            if (!Environment.IsProduction())
+            {
+                services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Title = "Vrum API",
+                        Description = "API",
+                        Version = "v1"
+                    });
+                });
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            if (!env.IsProduction())
+            {
+                app.UseSwagger();
+
+                app.UseSwaggerUI(swagger =>
+                {
+                    swagger.SwaggerEndpoint("/swagger/v1/swagger.json", "Vrum API V1");
+                    swagger.RoutePrefix = string.Empty;
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
